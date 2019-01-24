e98a3889d10440f2a06518218a3efb9459b4519e
Dmitry Minsky <me@dminsky.com>
2019-01-25 00:20:20 +0300

    Sphere diffuse material.

--- main.cpp
+++ main.cpp
@@ -5,10 +5,19 @@
 #include "float.h"
 #include "camera.h"
 
+vec3 random_in_unit_sphere() {
+	vec3 p;
+	do {
+		p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1); 
+	} while (p.squared_length() >= 1.0);
+	return p;
+}
+
 vec3 color(const ray& r, hitable *world) {
 	hit_record rec;
-	if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-		return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1 );
+	if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * color( ray(rec.p, target - rec.p), world );
 	} else {
 		vec3 unit_direction = unit_vector( r.direction() );
 		float t = 0.5 * ( unit_direction.y() + 1.0 );
@@ -19,7 +28,7 @@ vec3 color(const ray& r, hitable *world) {
 int main() {
 	int nx = 200;
 	int ny = 100;
-	int ns = 50;
+	int ns = 100;
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 	hitable *list[2];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5);
@@ -36,6 +45,8 @@ int main() {
 				col += color(r, world);
 			}
 			col /= float(ns);
+			// gamma correction by 1/2
+			col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
 			int ir = int(255.99 * col.r());
 			int ig = int(255.99 * col.g());
 			int ib = int(255.99 * col.b());
